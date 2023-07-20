3cd0fb49bd76872f9845dc6ac83e902a73313db1
Gerben Neven <gerbyzation@me.com>
2023-07-20 23:06:16 +0200

    add autopairs + format on save

--- init.lua
+++ init.lua
@@ -493,5 +493,7 @@ cmp.setup {
   },
 }
 
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
